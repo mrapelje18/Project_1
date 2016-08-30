c8045714644f60d733ab6102322afff2e7729d31
mrapelje18 <mrapelje18@windwardschool.org>
2016-08-30 08:26:47 -0700

    v2
    added some text

--- src/Class1.java
+++ src/Class1.java
@@ -4,7 +4,8 @@ public class Class1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello");
-		System.out.print("HI");
+		System.out.println("HI");
+		System.out.print("Heyo");
 	}
 
 }
