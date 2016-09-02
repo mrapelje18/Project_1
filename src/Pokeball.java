6c20635496862ff6e4fcbf31791fdb7dbaaf75a2
mrapelje18 <mrapelje18@windwardschool.org>
2016-09-02 08:44:39 -0700

    code for pokeball
    Prints pokeball and has comment

---
+++ src/Pokeball.java
@@ -0,0 +1,17 @@
+//This Pokeball graphic can be used in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon.
+
+public class Pokeball {
+	public static void main(String[] args)
+	{
+		System.out.println("      @@@@@@@@@@@@");
+		System.out.println("    @@@@@@@@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("|||||||||[\"\"\"\"]|||||||||");
+		System.out.println("|||||||||[....]|||||||||");
+		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOO");
+		System.out.println("  OOOOOOOOOOOOOOOOOOOO");
+		System.out.println("   OOOOOOOOOOOOOOOOO");
+		System.out.println("     OOOOOOOOOOOOO");
+	}
+}
