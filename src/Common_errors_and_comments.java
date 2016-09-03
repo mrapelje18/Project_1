7ae4af9f2a3d3f50f2aaa850fa476b436083f04d
mrapelje18 <mrapelje18@windwardschool.org>
2016-09-02 18:27:01 -0700

    errors and comments
    assignment to learn about comments and common errors in programming

---
+++ src/Common_errors_and_comments.java
@@ -0,0 +1,55 @@
+/* Comment Errors & Comments
+ * 	Micah Rapelje
+ * 	per.1
+ */
+
+
+//Q1 & Q2
+
+//This Pokeball graphic can be used in the opening sequence of my Pokemon game, or any time the player needs to catch a wild Pokemon.
+
+public class Common_errors_and_comments {
+	public static void main(String[] args)
+	{
+		System.out.println("      @@@@@@@@@@@@");
+		System.out.println("    @@@@@@@@@@@@@@@@");
+		System.out.println("  @@@@@@@@@@@@@@@@@@@@");
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.println("|||||||||[\"\"\"\"]|||||||||");
+		System.out.println("|||||||||[....]|||||||||");
+		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOO");
+		System.out.println("  OOOOOOOOOOOOOOOOOOOO");
+		System.out.println("   OOOOOOOOOOOOOOOOO");
+		System.out.println("     OOOOOOOOOOOOO");
+		
+/* Q3: 
+ * If you want to
+ * leave a note for future programmers
+ * in with your code
+ * place two slashes, like this // in front of each line of your comment
+ * Another alternative is to enclose your comments with
+ * /*, but not \\*!
+ */
+		
+/* Q4:
+ * Comments_for_future_programmers
+ * It sums up the main purpose of the program.
+ */
+		
+/* Q5: 
+ * public class SubwayAnnouncement {
+ * 		public static void main(String[] args)
+ * 		{
+ * 			System.out.println("Stand clear of");
+ *			System.out.println("the closing doors, please.");
+ * 		}
+ * }
+ */
+		
+/* Q6:
+ * Check for spelling errors
+ * Check heading for errors
+ * Check for correct punctuation (semicolons, brackets, quotations, spaces) 
+ */
+	}
+}
