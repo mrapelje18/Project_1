0d4805cad17791f64255dc5a00674af649e795a7
mrapelje18 <mrapelje18@windwardschool.org>
2016-09-07 09:29:02 -0700

    Pikachu chatter
    prints pikachu chattering
    uses one method

---
+++ src/PikachuChatter.java
@@ -0,0 +1,21 @@
+//This program prints Pikachu talking.  It has a method of printing Pikachu.
+
+public class PikachuChatter {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("	Pika pika pika chu pika chu peeeee ka pika chu!");
+		pikachu();
+		System.out.println("\n");
+		System.out.println("	Pika? Pika pika pika chu peeeee ka chu!");
+		pikachu();
+	}
+	
+	public static void pikachu() {
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
+	}
+
+
+}
